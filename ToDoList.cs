79e643698cedd172cfc7fd74566ecb9a2b52ce5f
Olle Frisk <olle.frisk.webd24jon@edu.tucsweden.se>
2025-02-21 16:17:57 +0100

    Created ToDoList class item

---
+++ ToDoList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAppWPF
+{
+    internal class ToDoList
+    {
+        private List<string> tasks = new();
+
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+        public void RemoveTask(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+            }
+        }
+        public List<string> GetAllTasks()
+        {
+            return tasks;
+        }
+
+    }
+}
