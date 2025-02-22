7009a26d487237ae714740b4c98ae8ae086b9dbd
Olle Frisk <olle.frisk.webd24jon@edu.tucsweden.se>
2025-02-22 18:03:46 +0100

    Updated logic in the MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,24 +16,40 @@ namespace ToDoAppWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ToDoList _todolist;
         public MainWindow()
         {
             InitializeComponent();
+            _todolist = new ToDoList();
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-
+            string task = TaskTextBox.Text;
+            if (!string.IsNullOrEmpty(task))
+            {
+                _todolist.AddTask(task);
+                UpdateTaskListBox();
+                TaskTextBox.Clear();
+            }
         }
 
         private void RemoveButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if(TaskListBox.SelectedIndex >= 0)
+            {
+                _todolist.RemoveTask(TaskListBox.SelectedIndex);
+                UpdateTaskListBox();
+            }
         }
 
         private void UpdateTaskListBox()
         {
-
+            TaskListBox.Items.Clear();
+            foreach (var task in _todolist.GetAllTasks())
+            {
+                TaskListBox.Items.Add(task);
+            }
         }
     }
 }
